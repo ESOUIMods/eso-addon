ac4f6e968f3ae7138a42270d02fce6e9c7a4804e
Sharlikran <danielhmpdx16@gmail.com>
2014-04-20 18:56:54 -0700

    Updated the debug message to show the item name

--- Esohead.lua
+++ Esohead.lua
@@ -360,7 +360,7 @@ function EH.OnLootReceived(eventCode, receivedBy, objectName, stackCount, soundC
         local link = EH.ItemLinkParse(objectName)
 
         if not EH.IsValidNode(targetName) then
-            EH.Debug("TargetName : " .. targetName .. " : ItemNumber : " .. link.id .. " was not found in EsoheadConstants.lua.")
+            EH.Debug("TargetName : " .. targetName .. ", contained : " .. link.name .. ", ItemNumber : " .. link.id .. ", was not found in EsoheadConstants.lua.")
             EH.Debug("Please report this in the Esohead forum.  Thank you.")
             return
         end
